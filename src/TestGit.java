a585f7309ad9fcf9a8b7b33caee08ee38205193a
FangXinZhen <1003340565@qq.com>
2019-05-09 14:13:15 +0800

    testgit

--- src/TestGit.java
+++ src/TestGit.java
@@ -3,8 +3,6 @@
  */
 public class TestGit {
     public static void main(String[] args) {
-        System.out.println("aa");
-        System.out.println("cc");
-        System.out.println("dd");
+        System.out.println("test");
     }
 }
